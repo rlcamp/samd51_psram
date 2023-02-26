60b1f4116591a53243e0ffcd134b8636e647bb9f
Richard Campbell <rlcamp@users.noreply.github.com>
2023-02-26 00:12:41 -0800

    rework example code showing the automatic start of enqueued write

--- samd51_psram.ino
+++ samd51_psram.ino
@@ -1,70 +1,77 @@
-#include <Arduino.h>
-
 #include "samd51_psram.h"
 
-#include <stdio.h>
-#include <string.h>
+static volatile char read_busy, write_busy;
+static char data_out[1024], data_in[1024];
 
-static const unsigned long interval_ms = 200;
-static unsigned long prev;
+static void write_start(void) {
+    static unsigned counter = 0;
+    snprintf(data_out, sizeof(data_out), "pass %u", counter);
+
+    const unsigned address = (counter++ * 1024) % 8388608;
+
+    const unsigned long micros_before_write = micros();
+
+    write_busy = 1;
+    psram_write(data_out, address, sizeof(data_out), &write_busy);
+
+    Serial.printf("%s: psram_write() returned in %lu us\r\n", __func__, micros() - micros_before_write);
+}
+
+static void write_finish(void) {
+    const unsigned long micros_before_spin = micros();
+    while (write_busy);
+    Serial.printf("%s: spun for %lu us\r\n", __func__, micros() - micros_before_spin);
+}
+
+static void read_start(void) {
+    /* put some text in the buffer to show if the transaction fails */
+    snprintf(data_in, sizeof(data_in), "fail");
+
+    static unsigned counter = 0;
+    const unsigned address = (counter++ * 1024) % 8388608;
+
+    const unsigned long micros_before_read = micros();
+
+    read_busy = 1;
+    psram_read(data_in, address, sizeof(data_in), &read_busy);
+
+    Serial.printf("%s: psram_read() returned in %lu us\r\n", __func__, micros() - micros_before_read);
+}
+
+static void read_finish(void) {
+    /* and sleep until it finishes */
+    const unsigned long micros_before_spin = micros();
+    while (read_busy);
+    Serial.printf("%s: spun for %lu us\r\n", __func__, micros() - micros_before_spin);
+
+    static unsigned counter = 0;
+    Serial.printf("%s: expected %u, read back: \"%s\"\r\n\r\n", __func__, counter++, data_in);
+}
 
 void setup() {
-    pinMode(LED_BUILTIN, OUTPUT);
-
     Serial.begin(115200);
     while (!Serial);
     Serial.printf("hello\r\n");
 
     psram_init();
 
-    prev = millis();
+    /* do one write before looping */
+    write_start();
 }
 
 void loop() {
-    const unsigned long now = millis();
-    if (now - prev < interval_ms) {
-        __WFI();
-        return;
-    }
-    prev += interval_ms;
-    /* get here on average every 200 milliseconds */
+    /* wait for previous write to finish (should not block due to the delay) */
+    write_finish();
 
-    static int state = 0;
-    state = !state;
+    /* initiate read of the previous write */
+    read_start();
 
-    if (1 == state) {
-        /* note this is static because it must have a longer lifetime than its scope */
-        static char data_out[1024];
-        static volatile char data_out_busy = 0;
+    /* immediately attempt to write - this should return immediately, having enqueued
+     the transaction such that the write starts when the read finishes */
+    write_start();
 
-        /* sleep until PREVIOUS transaction finishes before modifying buffer */
-        digitalWrite(LED_BUILTIN, HIGH);
-        while (data_out_busy) __WFI();
-        digitalWrite(LED_BUILTIN, LOW);
-        data_out_busy = 1;
+    /* wait for the read to finish */
+    read_finish();
 
-        /* put some text in the buffer */
-        static unsigned counter = 0;
-        snprintf(data_out, sizeof(data_out), "pass %u", counter++);
-
-        /* kick off the nonblocking transaction */
-        psram_write(data_out, 0, sizeof(data_out), &data_out_busy);
-    } else {
-        static char data_in[1024];
-        /* put some text in the buffer to show if the transaction fails */
-        snprintf(data_in, sizeof(data_in), "fail");
-
-        /* kick off the non blocking transaction... */
-        volatile char busy = 1;
-        psram_read(data_in, 0, sizeof(data_in), &busy);
-
-        /* do other work maybe */
-
-        /* and sleep until it finishes */
-        digitalWrite(LED_BUILTIN, HIGH);
-        while (busy) __WFI();
-        digitalWrite(LED_BUILTIN, LOW);
-
-        Serial.printf("read back: \"%s\"\r\n", data_in);
-    }
+    delay(1000);
 }
